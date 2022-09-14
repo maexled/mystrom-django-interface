53ed69dad8fc05d0fba1da6101c14f1596d6a44f
Max <max@dasmaexle.de>
2022-09-14 17:50:00 +0200

    change view name

--- shelly3em_rest/urls.py
+++ shelly3em_rest/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('devices/', views.device_list, name='rest_device_index'),
-    path('devices/<int:id>/', views.device_detail, name='rest_device_detail'),
-    path('devices/<int:id>/results/', views.device_results, name='rest_device_results'),
+    path('devices/', views.device_list, name='shelly_rest_device_index'),
+    path('devices/<int:id>/', views.device_detail, name='shelly_rest_device_detail'),
+    path('devices/<int:id>/results/', views.device_results, name='shelly_rest_device_results'),
 ]
