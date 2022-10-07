1ee9ce9754d1b53fd4ebb95536923d822323c882
Max <max@dasmaexle.de>
2022-10-07 23:42:24 +0200

    fix tests

--- interface/tests.py
+++ interface/tests.py
@@ -8,7 +8,7 @@ class MystromDevicesTestCase(TestCase):
         MystromDevice.objects.create(name="Device 2", ip="127.0.0.1")
 
     def test_create_device(self):
-        url = reverse('devices')
+        url = reverse('mystrom_devices')
         response = self.client.post(url, {'name': 'test', 'ip': '127.0.0.1'})
         self.assertContains(response, "test")
         self.assertContains(response, "127.0.0.1")
@@ -17,26 +17,26 @@ class MystromDevicesTestCase(TestCase):
         self.assertEqual(len(MystromDevice.objects.all()), 3)
     
     def test_create_device_fail_invalid_ip(self):
-        url = reverse('devices')
+        url = reverse('mystrom_devices')
         response = self.client.post(url, {'name': 'test', 'ip': 'Not an ip'})
         self.assertContains(response, "<div class=\"invalid-feedback\">Not valid IP Address</div>")
         self.assertEqual(len(MystromDevice.objects.all()), 2)
 
     def test_delete_devices(self):
-        url = reverse('devices')
+        url = reverse('mystrom_devices')
         response = self.client.delete(url)
         self.assertNotContains(response, "<tr>")
         self.assertEqual(len(MystromDevice.objects.all()), 0)    
         
     def test_get_create_device_form(self):
-        url = reverse('devices')
+        url = reverse('mystrom_devices')
         response = self.client.get(url)
-        self.assertContains(response, "Create device")
+        self.assertContains(response, "Create")
         self.assertContains(response, "<form")
         self.assertContains(response, "</form>")
 
     def test_update_device(self):
-        url = reverse('device', args=(1,))
+        url = reverse('mystrom_device', args=(1,))
         device = MystromDevice.objects.get(id=1)
 
         self.assertEqual(device.name, "Device 1")
@@ -47,20 +47,20 @@ class MystromDevicesTestCase(TestCase):
         self.assertEqual(device.name, "Not Device 1")
 
     def test_update_device_fail_invalid_ip(self):
-        url = reverse('device', args=(1,))
+        url = reverse('mystrom_device', args=(1,))
         response = self.client.post(url, {'name': 'Not Device 1', 'ip': 'Not an ip'})
         self.assertContains(response, "<div class=\"invalid-feedback\">Not valid IP Address</div>")
         self.assertEqual(len(MystromDevice.objects.all()), 2)
 
     def test_delete_device(self):
-        url = reverse('device', args=(1,))
+        url = reverse('mystrom_device', args=(1,))
         response = self.client.delete(url)
         self.assertContains(response, "<td>Device 2</td>")
         self.assertNotContains(response, "<td>Device 1</td>")
         self.assertEqual(len(MystromDevice.objects.all()), 1)
 
     def test_get_update_device_form(self):
-        url = reverse('device', args=(1,))
+        url = reverse('mystrom_device', args=(1,))
         response = self.client.get(url)
         self.assertContains(response, "Edit device")
         self.assertContains(response, "<form")
