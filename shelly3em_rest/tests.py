622cb50c97d93e444aa818813371b7cbae2f389f
maexled <max@maexled.de>
2024-07-13 14:36:00 +0200

    Implement export of power data

--- shelly3em_rest/tests.py
+++ shelly3em_rest/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+from .models import Shelly3EMDevice
+from .serializers import Shelly3EMDeviceSerializer
 
-# Create your tests here.
+class ShellyDeviceTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.url = reverse('shelly_rest_device_index')
+        self.device_data = {
+            'name': 'Test Device',
+            'ip': '192.168.0.74',
+            'active': True
+        }
+        self.device = Shelly3EMDevice.objects.create(
+            name='Test Device',
+            ip='192.168.0.74',
+            active=True
+        )
+
+    def test_get_device_list(self):
+        response = self.client.get(self.url)
+        devices = Shelly3EMDevice.objects.all()
+        serializer = Shelly3EMDeviceSerializer(devices, many=True)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.json(), serializer.data)
+
+    def test_create_device_invalid_data(self):
+        response = self.client.post(self.url, {})
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
