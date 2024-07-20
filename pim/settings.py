838806d0a9ccdff84946b6bc3b39a8d47d5617e9
maexled <max@maexled.de>
2024-07-20 21:29:53 +0200

    Mention that project only supports timescaledb as database

--- pim/settings.py
+++ pim/settings.py
@@ -124,31 +124,19 @@ if DEBUG:
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-ENGINETYPE = os.getenv("ENGINE_TYPE", "sqlite3")
-ENGINE = None
-if ENGINETYPE == "mysql":
-    ENGINE = "django.db.backends.mysql"
-elif ENGINETYPE == "postgresql":
-    ENGINE = "django.db.backends.postgresql_psycopg2"
+ENGINE = "django.db.backends.postgresql_psycopg2"
 
-if ENGINE:
-    DATABASES = {
-        "default": {
-            "ENGINE": ENGINE,
-            "NAME": os.environ["DB_NAME"],
-            "USER": os.environ["DB_USER"],
-            "PASSWORD": os.environ["DB_PASSWORD"],
-            "HOST": os.environ["DB_HOST"],
-            "PORT": os.environ["DB_PORT"],
-        }
+
+DATABASES = {
+    "default": {
+        "ENGINE": ENGINE,
+        "NAME": os.environ["DB_NAME"],
+        "USER": os.environ["DB_USER"],
+        "PASSWORD": os.environ["DB_PASSWORD"],
+        "HOST": os.environ["DB_HOST"],
+        "PORT": os.environ["DB_PORT"],
     }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
+}
 
 
 # Password validation
